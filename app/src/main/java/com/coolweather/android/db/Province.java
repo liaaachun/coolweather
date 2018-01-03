a80c38af4d769b8580609eb7b649cad1a7a55350
Lichun <lichun@gmail.com>
2018-01-03 14:50:36 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -8,7 +8,7 @@ import org.litepal.crud.DataSupport;
 
 public class Province extends DataSupport{
     private int id;
-    private String provinceNAME;
+    private String provinceName;
     private  int provinceCode;
 
     public int getId() {
@@ -24,11 +24,11 @@ public class Province extends DataSupport{
         this.provinceCode = provinceCode;
     }
 
-    public String getProvinceNAME() {
-        return provinceNAME;
+    public String getProvinceName() {
+        return provinceName;
     }
 
-    public void setProvinceNAME(String provinceNAME) {
-        this.provinceNAME = provinceNAME;
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
     }
 }
